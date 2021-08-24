91239177703d4d2c7b96676eabfe3da7d4f5649a
Ercüment Laçın <ercument.lacin@akinon.com>
2021-08-24 12:21:43 +0300

    new keys added

--- src/models/products.js
+++ src/models/products.js
@@ -13,6 +13,18 @@ const productsSchema = mongoose.Schema({
     type: Number,
     required: true,
   },
+  image: {
+    type: Array,
+    required: false,
+  },
+  category: {
+    type: String,
+    required: false,
+  },
+  color: {
+    type: String,
+    required: false,
+  },
 });
 
 module.exports = mongoose.model('Products', productsSchema);
